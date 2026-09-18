3631d4c692efc902e31aa4a9f9ac471a771ef4ef
lunaerem <140562305+lunaerem@users.noreply.github.com>
2026-09-18 15:10:01 -0500

    Rewrite how options are handled

--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <getopt.h>
 #include <stdio.h>
 #include <unistd.h>
 
@@ -9,32 +10,63 @@ int main(int argc, char *argv[]) {
 
   int opt;
   int rv = 0;
+  int opt_index = 0;
 
   // Checking that there is at least one argument
   if (argc < 2) {
-    fprintf(stderr, "Usage error: At least one argument must be given. See "
-                    "direct-dlp -h for details.\n");
+    // GUI not implemented yet
+    fprintf(stderr,
+            "Usage error: Only the CLI version of Direct-DLP is currently "
+            "supported as the GUI version is a work in progress. See "
+            "direct-dlp -h for details.\n");
     return -1;
   }
 
+  static struct option long_options[] = {{"version", no_argument, 0, 0},
+                                         {"help", no_argument, 0, 'h'}
+
+  };
+
   // Checking through the arguments
-  while ((opt = getopt(argc, argv, "vhld:")) != -1) {
+  while ((opt = getopt_long(argc, argv, "v:hla:c:", long_options,
+                            &opt_index)) != -1) {
+
     switch (opt) {
+    case 0:
+      printf("[Debug] Long option: %s\n", long_options[opt_index].name);
+      switch (opt_index) {
+      case 0:
+        printf("direct-dlp version: %s\n", VERSION);
+        return rv;
+        break;
+      default:
+        break;
+      }
+      break;
     case 'v':
-      printf("direct-dlp version: %s\n", VERSION);
+      printf("[Debug] Download Video: %s\n", optarg);
+      fprintf(stderr, "Currently not implemented yet, sorry.\n");
+      break;
       break;
     case 'l':
       rv = legacyui();
       break;
-    case 'd':
-      printf("Download: %s\n", optarg);
+    case 'a':
+      printf("[Debug] Download Audio: %s\n", optarg);
       fprintf(stderr, "Currently not implemented yet, sorry.\n");
       break;
     case 'h':
-      printf("Help\n");
+      printf("[Debug] Help\n");
+      fprintf(stderr, "Currently not implemented yet, sorry.\n");
+      break;
+    case 'c':
+      printf("[Debug] Configure: %s\n", optarg);
       fprintf(stderr, "Currently not implemented yet, sorry.\n");
       break;
     case '?':
+      break;
+    default:
+      printf("?? getopt returned character code 0%o ??\n", opt);
       break;
     }
   }
@@ -56,16 +88,16 @@ int legacyui() {
   // This will get cleaned up once things are rewritten for custom options
   // For now, this will stay pretty unoptimised and messy until then
   char *list[11] = {"python3",
-                   "yt-dlp",
-                   "-a",
-                   "list.txt",
-                   "-x",
-                   "--output=%(title)s.%(ext)s",
-                   "--audio-format=mp3",
-                   "--audio-quality=0",
-                   "--js-runtimes",
-                   "node",
-                   NULL};
+                    "yt-dlp",
+                    "-a",
+                    "list.txt",
+                    "-x",
+                    "--output=%(title)s.%(ext)s",
+                    "--audio-format=mp3",
+                    "--audio-quality=0",
+                    "--js-runtimes",
+                    "node",
+                    NULL};
 
   char *list2[10] = {
       "python3",
@@ -79,18 +111,19 @@ int legacyui() {
       "node",
       NULL};
 
-  char *list3[12] = {"python3",
-                    "yt-dlp",
-                    "-a",
-                    "list.txt",
-                    "--output=%(title)s.%(ext)s",
-                    "-f",
-                    "bestvideo[ext!=webm]+bestaudio[ext!=webm]/best[ext!=webm]",
-                    "--js-runtimes",
-                    "node",
-                    "--playlist-start",
-                    "210",
-                    NULL};
+  char *list3[12] = {
+      "python3",
+      "yt-dlp",
+      "-a",
+      "list.txt",
+      "--output=%(title)s.%(ext)s",
+      "-f",
+      "bestvideo[ext!=webm]+bestaudio[ext!=webm]/best[ext!=webm]",
+      "--js-runtimes",
+      "node",
+      "--playlist-start",
+      "210",
+      NULL};
 
   char *list4[4] = {"python3", "yt-dlp", "-U", NULL};
 
